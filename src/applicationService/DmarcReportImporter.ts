6f22b6b8377eb3b2077758480e15316491bcd03a
Natsuki DAIGO <leno@leno3s.net>
2024-09-02 05:22:26 +0900

    add: application service

---
+++ src/applicationService/DmarcReportImporter.ts
@@ -0,0 +1,21 @@
+import type { IDmarcReportRepository } from "../repository/IDmarcReportRepository";
+import type { IOutputTableRepository } from "../repository/IOutputTableRepository";
+
+export class DmarcReportImporter {
+  constructor(
+    private readonly inputRepository: IDmarcReportRepository,
+    private readonly outputRepository: IOutputTableRepository
+  ) {}
+
+  /**
+   * Import to output repository from xml reports.
+   */
+  execute() {
+    const reports = this.inputRepository.getDmarcReports();
+    console.log(`[DmarcReportImporter#execute] Loaded ${reports.length} reports.`)
+    if (reports.length <= 0) {
+      console.log("[DmarcReportImporter#execute] There are not new DMARC report. exit.");
+    }
+    this.outputRepository.save(reports);
+  }
+}
